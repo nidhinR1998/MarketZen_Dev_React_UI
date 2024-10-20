6166d8567ac6c712d5413115b5dd64035d483018
nidhinR1998 <nidhinrajesh1998@gmail.com>
2024-10-21 02:14:31 +0530

    Code Update/Adding

--- src/page/Wallet/Wallet.jsx
+++ src/page/Wallet/Wallet.jsx
@@ -1,7 +1,12 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { ReloadIcon } from '@radix-ui/react-icons'
-import { CopyIcon, DollarSign, WalletIcon } from 'lucide-react'
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
+import { ReloadIcon, ShuffleIcon, UpdateIcon } from '@radix-ui/react-icons'
+import { CopyIcon, DollarSign, UploadIcon, WalletIcon } from 'lucide-react'
 import React from 'react'
+import Topupform from './Topupform'
+import WithdrawalForm from './WithdrawalForm'
+import TransferForm from './TransferForm'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
 
 export const Wallet = () => {
   return (
@@ -18,15 +23,13 @@ export const Wallet = () => {
                     <p className="text-gray-200 text-sm">
                       #A475Ed
                     </p>
-                    <CopyIcon size={12}
-                      className="cursor-pointer hover:text-slate-300" />
+                    <CopyIcon size={12} className="cursor-pointer hover:text-slate-300" />
                   </div>
                 </div>
               </div>
               <div>
                 <ReloadIcon className="w-6 h-6 cursor-pointer hover:text-gray-400" />
               </div>
-
             </div>
           </CardHeader>
           <CardContent>
@@ -37,14 +40,99 @@ export const Wallet = () => {
               </span>
             </div>
             <div className="flex gap-7 mt-5">
+              <Dialog>
+                <DialogTrigger>
+                  <div className="h-24 w-24 hover:text-gray-400 cursor-pointer
+                    flex flex-col items-center justify-center rounded-md shadow-slate-800 shadow-md">
+                    <UploadIcon />
+                    <span className="text-sm mt-2">Add Money</span>
+                  </div>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>
+                      Top Up Your Wallet
+                    </DialogTitle>
+                  </DialogHeader>
+                  <Topupform />
+                </DialogContent>
+              </Dialog>
 
+              <Dialog>
+                <DialogTrigger>
+                  <div className="h-24 w-24 hover:text-gray-400 cursor-pointer
+                    flex flex-col items-center justify-center rounded-md shadow-slate-800 shadow-md">
+                    <UploadIcon />
+                    <span className="text-sm mt-2">Withdrawal</span>
+                  </div>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>
+                      Request Withdrawal
+                    </DialogTitle>
+                  </DialogHeader>
+                  <WithdrawalForm />
+                </DialogContent>
+              </Dialog>
+
+              <Dialog>
+                <DialogTrigger>
+                  <div className="h-24 w-24 hover:text-gray-400 cursor-pointer
+                    flex flex-col items-center justify-center rounded-md shadow-slate-800 shadow-md">
+                    <ShuffleIcon />
+                    <span className="text-sm mt-2">Transfer</span>
+                  </div>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle className="text-center text-xl">
+                      Transfer To Other Withdrawal
+                    </DialogTitle>
+                  </DialogHeader>
+                  <TransferForm />
+                </DialogContent>
+              </Dialog>
             </div>
           </CardContent>
         </Card>
+        <div className="py-5 pt-10">
+          <div className="flex gap-2 items-center pb-5">
+            <h1 className="text-2xl font-semibold">History</h1>
+            <UpdateIcon className="h-7 w-7 p-0 cursor-pointer hover:text-gray-400"/>
 
+          </div>
+
+          <div className="space-y-5">
+
+            <div>
+              <Card className="lg:w-[50%] px-5 flex justify-between items-center p-2">  ///3.24.10
+                <div className="flex items-center gap-5">
+                  <Avatar>
+                   <AvatarFallback>
+                   <ShuffleIcon className=""/>
+                   </AvatarFallback>
+                  </Avatar>
+
+                  <div className="space-y-1">
+                    <h1>Buy Asset</h1>
+                    <p className="text-sm text-gray-500">2024-06-02</p>
+
+                  </div>
+
+                </div>
+
+              </Card>
+            </div>
+
+          </div>
+
+        </div>
       </div>
     </div>
   )
 }
+
+
 
 export default Wallet
