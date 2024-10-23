251fee0ee5f63e896e6bf7c7dcbb1714005e3ab7
nidhinR1998 <nidhinrajesh1998@gmail.com>
2024-10-24 02:26:19 +0530

    UI Design is Completeed for the Backend Integration

--- src/page/Auth/Auth.jsx
+++ src/page/Auth/Auth.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import { useLocation, useNavigate } from "react-router-dom"
+import "./Auth.css"
+import SignupForm from "./SignupForm"
+import { Button } from "@/components/ui/button"
+import ForgotPasswordForm from "./ForgotPasswordForm"
+import SigninForm from "./SigninForm"
 
 const Auth = () => {
+
+  const navigate=useNavigate()
+  const location=useLocation()
+
   return (
-    <div>Auth</div>
+    <div className='h-screen relative authContainer'>
+      <div className='absolute top-0 right-0 left-0 bottom-0 bg-[#030712] bg-opacity-50'>
+
+        <div className='bgBlure absolute top-1/2 left-1/2 transform 
+        -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center 
+        items-center h-[35rem] w-[30rem] rounded-md z-50 bg-black 
+        bg-opacity-50 shadow-2xl shadow-white px-10'>
+
+          <h1 className="text-6xl font-bold pb-9">Market Zen</h1>
+
+          {location.pathname=="/signup"  ?  (
+            <section className="w-full">
+            <SignupForm/>
+            <div className="flex items-center justify-center">
+              <span>Have already account ?</span>
+              <Button onClick={()=>navigate("/sigin")} variant="ghost">
+                SingIn
+              </Button>
+
+            </div>
+          </section>): location.pathname=="/forgot-password"?(<section className="w-full">
+<ForgotPasswordForm/>
+<div className="flex items-center justify-center mt-2">
+              <span>Back to Login ?</span>
+              <Button onClick={()=>navigate("/sigin")} variant="ghost">
+                SingIn
+              </Button>
+
+            </div>
+          </section>):(<section className="w-full">
+            <SigninForm/>
+            <div className="flex items-center justify-center">
+              <span>{"Don't have account ?"}</span>
+              <Button onClick={()=>navigate("/signup")} variant="ghost">
+                SingUn
+              </Button>
+
+            </div>
+
+            <div className="mt-10">
+              
+              <Button className="w-full py-5" onClick={()=>navigate("/forgot-password")} variant="outline">
+                Forgot Password
+              </Button>
+
+            </div>
+            </section>)}
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
